de8097d3cacf1df197b52e9f785145317eee517a
Nine <cuz_mr.j@live.com>
2025-11-28 16:16:13 +0800

    错误修复

--- Nine.Design.Login/Helpers/HttpHelper.cs
+++ Nine.Design.Login/Helpers/HttpHelper.cs
@@ -7,27 +7,25 @@ namespace Nine.Design.Login.Helpers
 {
     public static class HttpHelper
     {
+        // .NET 4.6+ 和 .NET Core 都支持 HttpClient 的无参构造函数
+        // 建议使用 static readonly 来创建一个单例实例，以优化性能和资源使用
         private static readonly HttpClient _httpClient = new HttpClient();
 
         /// <summary>
-        /// 跨框架 GET 请求
+        /// 发送 GET 请求并返回响应内容的字符串形式。
         /// </summary>
+        /// <param name="url">请求的 URL。</param>
+        /// <returns>一个包含响应内容的 Task。</returns>
         public static async Task<string> GetAsync(string url)
         {
-#if NET45
-            // .NET 4.5 不支持 async/await，用 ContinueWith 链式调用
-            return await _httpClient.GetAsync(url)
-                .ContinueWith(task =>
-                {
-                    var response = task.Result;
-                    response.EnsureSuccessStatusCode();
-                    return response.Content.ReadAsStringAsync().Result;
-                });
-#else
-            var response = await _httpClient.GetAsync(url);
+            // 直接使用标准的 async/await 模式，所有支持的框架都能理解
+            var response = await _httpClient.GetAsync(url).ConfigureAwait(false);
+
+            // 如果 HTTP 响应状态码不是成功的（2xx），则抛出异常
             response.EnsureSuccessStatusCode();
-            return await response.Content.ReadAsStringAsync();
-#endif
+
+            // 异步读取响应内容
+            return await response.Content.ReadAsStringAsync().ConfigureAwait(false);
         }
     }
 }
